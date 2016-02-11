94e2c3c2b285cae109d9fdcbe0760b9e43d2e954
alanlv200 <vistlv200@gmail.com>
2016-02-12 01:10:36 +0900

    retry

--- GachaTech/ViewController.swift
+++ GachaTech/ViewController.swift
@@ -20,6 +20,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func gacha() {
+        self.performSegueWithIdentifier("result", sender: nil)
+    }
+    
+    
 
 }
 
--- ResultViewController.swift
+++ ResultViewController.swift
@@ -9,16 +9,65 @@
 import UIKit
 
 class ResultViewController: UIViewController {
+    
+    @IBOutlet var haikeiImageView: UIImageView!
+    @IBOutlet var monsterImageView: UIImageView!
+    var monsterArray: [UIImage]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let number = Int(rand() % 9)
+        
+        monsterArray = [UIImage(named: "monster001.png")!,
+            UIImage(named: "monster002.png")!,
+            UIImage(named: "monster003.png")!,
+            UIImage(named: "monster004.png")!,
+            UIImage(named: "monster005.png")!,
+            UIImage(named: "monster006.png")!,
+            UIImage(named: "monster007.png")!,
+            UIImage(named: "monster008.png")!,
+            UIImage(named: "monster009.png")!,
+            UIImage(named: "monster010.png")!
+        ]
+        
+        monsterImageView.image = monsterArray[number]
+        
+        if number == 9 {
+            haikeiImageView.image = UIImage(named: "bg_gold.png")
+        }else if number > 6 {
+            haikeiImageView.image = UIImage(named: "bg_red.png")
+        }else {
+            haikeiImageView.image = UIImage(named: "bg_blue.png")
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(true)
+        
+        let animation = CABasicAnimation(keyPath: "transform")
+        
+        animation.fromValue = NSNumber(double: 0)
+        
+        animation.toValue = NSNumber(double: 2 * M_PI)
+        
+        animation.valueFunction = CAValueFunction(name: kCAValueFunctionRotateZ)
+        
+        animation.duration = 5
+        
+        animation.repeatCount = Float.infinity
+        
+        haikeiImageView.layer.addAnimation(animation, forKey: nil)
+    }
+    
+    @IBAction func modoru() {
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
     
 
